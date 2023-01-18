ddfaab876975c4f838fe35460042d492f6e2b0c8
juanh <612110441@utsalamanca.edu.mx>
2023-01-18 10:18:11 -0600

    nos

---
+++ layouts/index.vue
@@ -0,0 +1,19 @@
+<template>
+    <v-row justify="center" aling="center" class="renglon">
+        <nuxt />
+    </v-row>
+</template>
+
+<style>
+
+.renglon
+{
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+}
+
+</style>

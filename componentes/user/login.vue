a8c2b9966ff6a5300ceecac9e0c32b040e4cccb8
juanh <612110441@utsalamanca.edu.mx>
2023-01-18 10:18:57 -0600

    nuxt

---
+++ componentes/user/login.vue
@@ -0,0 +1,36 @@
+<template>
+    <v-card > </v-card>
+    <v-card>
+<v-card-title>login</v-card-title>
+    </v-card>
+    <v-form v-model="formularioValido">
+        <v-text-field label="correo" placeholder="correo">
+            v-model="correo"
+        </v-text-field>
+    </v-form>
+ <v-card-actions>
+    Boton para iniciar sesion
+ </v-card-actions>
+</template>
+
+<script>
+exports default{
+    Data(){
+        return{
+                formularioValido:false,
+                correo:false
+        }
+    }
+}
+</script>
+<style scoped>
+
+.cardlogin{
+    background-color: bisque;
+    border-radius: 10px;
+    width: 500px;
+    height: auto;
+}
+
+
+</style>
---
+++ index.vue
@@ -0,0 +1,18 @@
+<template>
+   <login />
+</template>
+
+<script>
+export default{
+    name: 'Index page',
+    layout: 'login',
+    components:{
+        login
+    }
+}</script>
+
+<script>
+    import login from './components/user/login.vue'
+</script>
+
+<style scoped></style>
